4fdc1216a1edd58fa2f46a0b43480b9ed93a96b7
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-12-31 01:20:10 +0700

    elfinsanjaya:release v.1.0 boilerpalte express js typescript

--- tests/hello-world.test.ts
+++ tests/hello-world.test.ts
@@ -4,6 +4,6 @@ import { hello } from '../hello-world';
 describe('Hello function', () => {
   it('should return hello world', () => {
     const result = hello();
-    expect(result).to.equal('Add Configuration Travis CI');
+    expect(result).to.equal('Add Configuration Travis CI & Install Redis In Travis CI');
   });
 });
